53b6569a38247bd10ac2d4a0c83258908c3426b8
Arif <arifmollick8578@gmail.com>
2023-02-02 14:43:18 +0530

    HEAD with structured directories

---
+++ app/src/main/java/com/mollick/cryptocurrencywikiapp/presentation/MainActivity.kt
@@ -0,0 +1,28 @@
+package com.mollick.cryptocurrencywikiapp.presentation
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Surface
+import androidx.compose.ui.Modifier
+import com.mollick.cryptocurrencywikiapp.presentation.ui.theme.CryptoCurrencyWikiAppTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            CryptoCurrencyWikiAppTheme {
+                // A surface container using the 'background' color from the theme
+                Surface(
+                    modifier = Modifier.fillMaxSize(),
+                    color = MaterialTheme.colors.background
+                ) {
+
+                }
+            }
+        }
+    }
+}
+
