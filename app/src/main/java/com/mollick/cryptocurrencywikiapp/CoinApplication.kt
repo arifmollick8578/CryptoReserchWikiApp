e558f430dbce62daadbdc93406e119744dbd81ab
Arif <arifmollick8578@gmail.com>
2023-02-02 16:57:03 +0530

    Set up dependency injection and provide from application.

---
+++ app/src/main/java/com/mollick/cryptocurrencywikiapp/CoinApplication.kt
@@ -0,0 +1,7 @@
+package com.mollick.cryptocurrencywikiapp
+
+import android.app.Application
+import dagger.hilt.android.HiltAndroidApp
+
+@HiltAndroidApp
+class CoinApplication: Application()
--- app/src/main/java/com/mollick/cryptocurrencywikiapp/common/Constants.kt
+++ app/src/main/java/com/mollick/cryptocurrencywikiapp/common/Constants.kt
@@ -1,5 +1,5 @@
 package com.mollick.cryptocurrencywikiapp.common
 
 object Constants {
-
+    const val BASE_URL = "https://api.coinpaprika.com/"
 }
