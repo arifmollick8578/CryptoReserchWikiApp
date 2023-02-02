22723564a8343a377f8e333b2d3ad530bc8af251
ARIF MOLLICK <52036685+arifmollick8578@users.noreply.github.com>
2023-02-02 15:38:05 +0530

    Implement API and model class (#2)
    * Implementing API and model class.
    
    * Implementing API and model class.

---
+++ app/src/main/java/com/mollick/cryptocurrencywikiapp/data/remote/dto/TeamMember.kt
@@ -0,0 +1,7 @@
+package com.mollick.cryptocurrencywikiapp.data.remote.dto
+
+data class TeamMember(
+    val id: String,
+    val name: String,
+    val position: String
+)
---
+++ app/src/main/java/com/mollick/cryptocurrencywikiapp/data/remote/dto/Whitepaper.kt
@@ -0,0 +1,6 @@
+package com.mollick.cryptocurrencywikiapp.data.remote.dto
+
+data class Whitepaper(
+    val link: String,
+    val thumbnail: String
+)
